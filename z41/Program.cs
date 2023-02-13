2ecbd540042c284479e6e59cdee84c1583b6c1d3
Stanislav Shishkin <shishkinstas@mail.ru>
2023-02-13 16:51:55 +0700

    first commit

---
+++ z41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите числа. Для остановки ввода - введите 0");
+int n = Convert.ToInt32(Console.ReadLine()), m = 0;
+if (n > 0)
+    m = m +1;
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > 0)
+    m = m +1;
+}
+Console.WriteLine(m);
+
---
+++ z43/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите значение К1");
+
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение B1");
+
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение К2");
+
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение B2");
+
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+
+double y = k1 * x + b1;
+
+Console.WriteLine($"Координаты пересечения ({x}, {y})");
